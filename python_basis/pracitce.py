36e573540d33514b5f03031b83d9c6eaf315cf6e
vyu <vyuvyuvyu@naver.com>
2022-02-28 23:54:04 +0900

    basis step check2

--- python_basis/pracitce.py
+++ python_basis/pracitce.py
@@ -160,7 +160,7 @@ print(string1.find("n"))
 print(string1.find("Java"))
 # print(string1.index("Java"))
 
-# find도 indeex도 문자를 찾아주지만
+# find도 index도 문자를 찾아주지만
 # find는 없으면 -1, index는 없으면 error
 
 print(string1.count("n")) # n이 몇 번 등장하는지
@@ -231,6 +231,19 @@ password = domain_name[:3] + str(len(domain_name))+ str(domain_name.count("e")) 
 print(f"{domain_example}의 비밀번호는 {password}입니다")
 """
 
+
+# 2022/02/28 try
+# point : no check, it's count
+"""
+url_temp = "http://naver.com"
+url_temp = url_temp.replace("http://","")
+
+string_temp = url_temp[:url_temp.index(".")]
+string_output = string_temp[:3] + str(len(string_temp)) + str(string_temp.count("e")) +'!'
+print(string_output)
+"""
+
+
 ### Quiz3
 # 사이트별로 비밀번호를 만들어 주는 프로그램 작성
 
@@ -241,3 +254,161 @@ print(f"{domain_example}의 비밀번호는 {password}입니다")
 #                       (nav)         (5)           (1)          (!)
 
 # ex) 생성된 비밀번호 : nav51!
+
+
+#################### 자료구조
+### 리스트 []
+"""
+# 지하철 칸별로 10명, 20명, 30명
+subway = [10,20,30]
+print(subway)
+
+subway = ["유재석", "조세호", "박명수"]
+print(subway)
+
+# 조세호씨가 몇 번째 칸인지
+# index
+print(subway.index("조세호"))
+
+# 하하씨가 다음 정류장에 다음 칸에 탐
+subway.append("하하")
+print(subway)
+
+# 정형돈씨를 유재석 / 조세호 사이에
+subway.insert(1, "정형돈")
+print(subway)
+
+# 지하철에 있는 사람을 한 명씩 뒤에서 꺼냄
+print(subway.pop())
+print(subway)
+
+print(subway.pop())
+print(subway)
+
+print(subway.pop())
+print(subway)
+
+# 같은 이름의 사람이 몇 명 있는지 확인
+subway.append("유재석")
+print(subway)
+print(subway.count("유재석"))
+
+# 정렬도 가능
+num_list = [5,2,4,3,1]
+num_list.sort()
+print(num_list)
+
+# 순서 뒤집기
+num_list.reverse()
+print(num_list)
+
+# 모두 지우기
+num_list.append(2.2)
+num_list.sort()
+print(num_list)
+
+# 다양한 자료형 함께 사용 가능 !
+mix_list = ["조세호", 20, True]
+print(mix_list)
+
+# 리스트 확장
+num_list.extend(mix_list)
+print(num_list)
+
+### 사전(dictionary..인데 명령어는 cabinet)
+cabinet = {3:"유재석", 100:"김태호"}
+print(cabinet[3])
+print(cabinet[100])
+
+print(cabinet.get(3))
+
+# index range 벗어나는 경우
+# print(cabinet[5]) # 오류나고 프로그램 종료
+print(cabinet.get(5)) # None 출력
+print(cabinet.get(5,"사용 가능")) # 이런 식으로도 사용 가능
+
+print(3 in cabinet) # True
+print(5 in cabinet) # False
+
+cabinet = {"A-3":"유재석", "B-100":"김태호"}
+print(cabinet["A-3"])
+print(cabinet["B-100"])
+
+# 새 손님
+print(cabinet)
+cabinet["A-3"] = "김종국"
+cabinet["C-20"] = "조세호"
+print(cabinet)
+
+# 간 손님
+del cabinet["A-3"]
+print(cabinet)
+
+# key들만 출력
+print(cabinet.keys())
+
+# value들만 출력
+print(cabinet.values())
+
+# key, value 쌍으로 출력
+print(cabinet.items())
+
+# 목욕탕 폐점
+cabinet.clear()
+print(cabinet)
+
+### 튜플
+# 내용 변경이나 추가를 할 수 없는데 속도가 리스트보다 빠르다
+# 변경되지 않는 경우, 튜플 사용이 유리
+
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+# menu.add("생선까스") # error
+
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
+print(name)
+
+### 집합(set)
+# 중복 안 됨, 순서 없음
+my_set = {1,2,3,3,3}
+print(my_set)
+
+java = {"유재석", "김태호", "양세형"}
+python = set(["유재석", "박명수"])
+
+# 교집합 (java와 python을 모두 할 수 있는 개발자)
+print(java & python)
+print(java.intersection(python))
+
+# 합집합 (java 할 수 있거나 python 할 수 있는 개발자)
+print(java | python)
+print(java.union(python))
+
+# 차집합 (java 할 수 있지만 python은 할 줄 모르는 개발자)
+print(java - python)
+print(java.difference(python))
+
+# python 할 줄 아는 사람이 늘어남
+python.add("김태호")
+print(python)
+
+# java를 까먹음
+java.remove("김태호")
+print(java)
+
+"""
+### 자료구조의 변경
+# 커피숍
+menu = {"커피", "우유", "주스"}
+print(menu, type(menu))
+
+menu = list(menu)
+print(menu, type(menu))
+
+menu = tuple(menu)
+print(menu, type(menu))
+
+menu = set(menu)
+print(menu, type(menu))
