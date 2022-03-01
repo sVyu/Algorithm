f09a66cb21b0bf53fdca6b475b592ab78bb9471d
vyu <vyuvyuvyu@naver.com>
2022-03-01 23:37:24 +0900

    basis step check 3

--- python_basis/pracitce.py
+++ python_basis/pracitce.py
@@ -398,9 +398,8 @@ print(python)
 java.remove("김태호")
 print(java)
 
-"""
 ### 자료구조의 변경
-# 커피숍
+# ex) 커피숍
 menu = {"커피", "우유", "주스"}
 print(menu, type(menu))
 
@@ -412,3 +411,247 @@ print(menu, type(menu))
 
 menu = set(menu)
 print(menu, type(menu))
+
+# Quiz) 당신의 학교에서 파이썽 코딩 대회를 주최합니다
+# 참석률을 높이기 위해 댓글 이벤트를 진행
+# 댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰
+# 추천 프로그램을 작성 ~
+
+# 조건1 : 편의상 댓글은 20명이 작성하였고 아이디는 1~20이라고 가정
+# 조건2 : 댓글 내용과 상관 없이 무작위로 추첨하되 중복 불가
+# 조건3 : random 모듈의 shuffle과 sample 활용
+
+# 출력 예제
+# -- 당첨자 발표 --
+# 치킨 당첨자 : 1
+# 커피 당첨자 : [2,3,4]
+# -- 축하합니다 --
+
+# 활용 예제
+# from random import *
+# lst = [1,2,3,4,5]
+# print(lst)
+
+# shuffle(lst)
+# print(lst)
+# print(sample(lst, 1)) #lst에서 1개만큼 뽑음
+
+# Do it !!
+# my solution
+# from random import *
+# lst = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# print(type(lst))
+
+# shuffle(lst)
+# print(lst)
+
+# # 치킨 1명
+# # print(sample(lst, 1))
+# winner_chicken = sample(lst, 1)
+# print(winner_chicken)
+
+# lst_set = set(lst)
+# # print(lst_set)
+
+# lst_set = lst_set.difference(set(winner_chicken))
+# # print(lst_set)
+# lst = list(lst_set)
+
+# # 이거 아니야 ~
+# # lst.reverse()
+# # lst.pop()
+# # lst.reverse()
+
+# # 커피 쿠폰 3명
+# # print(sample(lst, 3))
+# winner_coffee = sample(lst, 3)
+
+# print(f'''
+# -- 당첨자 발표 --
+# 치킨 당첨자 : {winner_chicken}
+# 커피 당첨자 : {winner_coffee}
+# -- 축하합니다 --
+# ''')
+
+# 풀이
+from random import *
+users = range(1, 21) # 1부터 20까지 생성  # type : <class 'range'>
+users = list(users)
+
+# print(users)
+shuffle(users)
+# print(users)
+
+winners = sample(users, 4) # 1명 치킨 + 3명 커피
+
+print(" -- 당첨자 발표 --")
+print("치킨 당첨자 : {0}".format(winners[0]))
+print("커피 당첨자 : {0}".format(winners[1:]))
+print("-- 축하합니다 --")
+
+# Quiz) 당신의 학교에서 파이썽 코딩 대회를 주최합니다
+# 참석률을 높이기 위해 댓글 이벤트를 진행
+# 댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰
+# 추천 프로그램을 작성 ~
+
+# 조건1 : 편의상 댓글은 20명이 작성하였고 아이디는 1~20이라고 가정
+# 조건2 : 댓글 내용과 상관 없이 무작위로 추첨하되 중복 불가
+# 조건3 : random 모듈의 shuffle과 sample 활용
+
+# day2
+from random import *
+lst = range(1,21)
+lst = list(lst)
+
+shuffle(lst)
+# print(lst)
+
+lst_winner = sample(lst,4)
+print(lst_winner)
+
+print(f'''
+-- 당첨자 발표 --
+치킨 당첨자 : {lst_winner[0]}
+커피 당첨자 : {lst_winner[1:]}
+-- 축하합니다 -- 
+''')
+
+"""
+# Quiz) 당신의 학교에서 파이썽 코딩 대회를 주최합니다
+# 참석률을 높이기 위해 댓글 이벤트를 진행
+# 댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰
+# 추천 프로그램을 작성 ~
+
+# 조건1 : 편의상 댓글은 20명이 작성하였고 아이디는 1~20이라고 가정
+# 조건2 : 댓글 내용과 상관 없이 무작위로 추첨하되 중복 불가
+# 조건3 : random 모듈의 shuffle과 sample 활용
+
+
+#################### 제어문
+"""
+### if
+
+# weather = '맑아요'
+# weather = input("오늘 날씨는 어때요? ")
+# if weather == '비' or weather == '눈':
+#     print("우산을 챙기세요")
+# elif weather == '미세먼지':
+#     print('마스크를 챙기세요')
+# else:
+#     print('준비물 필요 없어요')
+
+# temp = int(input("기온은 어때요? "))
+# if 30 <= temp:
+#     print("너무 더워요. 나가지 마세요")
+# elif 10 <= temp and temp <30:
+#     print("괜찮은 날씨예요")
+# elif 0 <= temp <10:
+#     print("외투를 챙기세요")
+# else:
+#     print("너무 추워요. 나가지 마세요")
+
+
+# for
+
+# for waiting_no in [0,1,2,3,4]:
+# for waiting_no in range(5):   # 0 ~ 4
+# for waiting_no in range(1,6): # 1 ~ 5
+    # print("대기번호 : {0}" .format(waiting_no))
+
+# starbucks = ["아이언맨","토르", "아이엠 그루트"]
+# for customer in starbucks:
+#     print("{0}, 커피가 준비되었습니다." .format(customer))
+
+
+# while
+
+# customer = "토르"
+# index = 5
+# while index >= 1:
+#     print("{0}, 커피가 준비 되었습니다. {1}번 남았어요".format(customer,index))
+#     index -= 1
+#     if index ==0:
+#         print("커피는 폐기처분되었습니다.")
+
+# (roop)
+# customer = "아이언맨"
+# index = 1
+# while True:
+#     print("{0}, 커피가 준비 되었습니다. 호출{1}회" .format(customer, index))
+#     index += 1
+
+# customer = "토르"
+# person = "Unknown"
+# while person != customer :
+#     print("{0}, 커피가 준비 되었습니다." .format(customer))
+#     person = input("이름이 어떻게 되세요? ")
+
+# continue와 break
+# absent = [2, 5] # 결석
+# no_book = [7] # 책을 깜빡했음
+# for student in range(1, 11): # 1,2,3,4,5,6,7,8,9,10,11
+#     if student in absent:
+#         continue
+#     elif student in no_book:
+#         print("오늘 수업 여기까지. {0}는 교무실로 따라와" .format(student))
+#         break
+#     print("{0}, 책을 읽어봐".format(student))
+
+
+# 한 줄 for
+# 출석번호가 1 2 3 4, 앞에 100을 붙이기로 함 → 101, 102, 103, 104.
+# students = [1,2,3,4,5]
+# print(students)
+# students = [i+100 for i in students]
+# print(students)
+
+# 학생 이름을 길이로 변환
+# my code
+# students = ["Iron man", "Thor", "I am groot"]
+# len_name = [len(name_of_students) for name_of_students in students]
+# print(len_name)
+
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [len(i) for i in students]
+# print(students)
+
+# 모두 대문자로 변환
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [i.upper() for i in students]
+# print(students)
+
+"""
+
+# Quiz) 당신은 Cocoa 서비스를 이용하는 택시 기사
+# 50명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램을 작성
+
+# 조건 1 : 승객별 운행 소요 시간은 5분 ~ 50분 사이의 난수로 정해집니다.
+# 조건 2 : 당신은 소요 시간 5분 ~ 15분 사이의 승객만 매칭해야 합니다.
+
+# (출력문 예제)
+# [O] 1번째 손님 (소요시간 : 15분)
+# [ ] 2번째 손님 (소요시간 : 50분)
+# [O] 3번째 손님 (소요시간 : 5분)
+# ...
+# [ ] 50번째 손님 (소요시간 : 16분)
+
+# 총 탑승 승객 : 2 분
+
+# my_code
+from random import *
+total_passenger = 0
+list_passenger = []
+for i in range(1,51):   # no 50, it's 51
+    drival_time = int(5 + random()*45)  # randrange(5, 51)
+    # print(drival_time)
+
+    if drival_time <= 15:   # 어차피 최소 시간이 5분이라 15분 이하인 조건만
+        check = "O"
+        total_passenger += 1
+        list_passenger.append(i)
+    else:
+        check = " "
+    print('[{0}] {1}번째 손님 (소요시간 : {2}분)'.format(check, i, drival_time))
+print("총 탑승 승객 : {0} 분".format(total_passenger))
+print(list_passenger)
+
