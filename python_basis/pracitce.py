52317389a09a6ada21ef2447703b2bc128eccab5
vyu <vyuvyuvyu@naver.com>
2022-03-03 23:53:42 +0900

    basis step check5

--- python_basis/pracitce.py
+++ python_basis/pracitce.py
@@ -784,19 +784,351 @@ print("남은 총 : {0}" .format(gun))
 # 출력 예제
 # 키 175cm 남자의 표준 체중은 67.38kg 입니다.
 
-def std_weight(height, gender):
-    if(gender=='male'):
-        bmi_weight = 22
-    elif(gender=='female'):
-        bmi_weight = 21
 
-    height_m = height/100
+# def std_weight(height, gender):
+#     if(gender=='male'):
+#         bmi_weight = 22
+#     elif(gender=='female'):
+#         bmi_weight = 21
 
-    return height_m * height_m * bmi_weight
+#     height_m = height/100
 
-sample_man_height = 175
-gender = "male"
-print("키 {0}cm 남자의 표준 체중은 {1}kg 입니다."\
-    .format(sample_man_height, round(std_weight(sample_man_height, 'male'),2) ))
+#     return height_m * height_m * bmi_weight
+
+# sample_man_height = 175
+# gender = "male"
+# print("키 {0}cm 남자의 표준 체중은 {1}kg 입니다."\
+#     .format(sample_man_height, round(std_weight(sample_man_height, 'male'),2) ))
 
 # 반올림은 round 함수
+
+
+#################### 입출력
+"""
+### 표준입출력
+
+# print("Python", "Java", sep=", ")
+# print("Python", "Java", "JavaScript", sep=" vs ", end="? ")
+# print("무엇이 더 재밌을까요?")
+
+# import sys
+# print("Python", "Java", file=sys.stdout)
+# print("Python", "Java", file=sys.stderr)
+
+# 시험 성적
+# .ljust, .rjust
+# scores = {"수학":0, "영어":50, "코딩":100}
+# for subject, score in scores.items():
+#     # print(subject, score)
+#     print(subject.ljust(5), str(score).rjust(4), sep=":")
+
+# 은행 대기순번표
+# .zill
+# 001, 002, 003, ...
+# for num in range(1, 21):
+    # print("대기번호 : " + str(num).zfill(3))
+
+# input으로 사용자 입력 저장 시 항상 문자열(str)로 처리됨
+# answer = input("아무 값이나 입력하세요 : ")
+# print("입력하신 값은 " + answer + "입니다.")
+
+
+### 다양한 출력포맷
+# 빈 자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+print("{0:>10}" .format(500))
+
+# 양수일 땐 +로 표시, 음수일 땐 -로 표시
+print("{0: >+10}" .format(500))
+print("{0: >+10}" .format(-500))
+
+# 왼쪽 정렬하고, 빈칸을 _로 채움
+print("{0:_<+10}".format(500))
+print("{0:_<10}".format(500))
+
+#3자리마다 콤마 찍어주기
+print("{0:,}".format(100000000000))
+
+#3자리마다 콤마 찍어주기, +- 부호 붙이기
+print("{0:+,}".format(100000000000))
+print("{0:,}".format(-100000000000))
+
+# 3자리마다 콤마를 찍고, 부호도 붙이고, 자리수 확보
+# 돈이 많으면 행복하니 빈 자리는 ^로 채우기
+print("{0:^<+30,}" .format(100000000))
+
+# 소수점 출력
+print("{0:f}".format(5/3))
+
+# 소수점 특정 자리수 까지만 표시 (소수점 3째 자리에서 반올림)
+print("{0:.2f}".format(5/3))
+
+### 파일입출력
+# score_file = open("score.txt", "w", encoding="utf-8")
+# print("수학 : 0", file=score_file)
+# print("영어 : 50",file=score_file)
+# score_file.close()
+
+# score_file = open("score.txt", "a", encoding="utf8")
+# score_file.write("과학 : 90")
+# score_file.write("\n코딩 : 100")
+# score_file.close()
+
+# 한번에 read
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.read())
+# score_file.close()
+
+# 한 줄씩 read
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.readline(), end="") # 줄별로 읽기, 한 줄 읽고 커서는 다음 줄로
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# score_file.close()
+
+# 한 줄씩 정석적인 방법
+# score_file = open("score.txt", "r", encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+# score_file.close()
+
+# 한 줄씩 정석적인 방법2
+# score_file = open("score.txt", "r", encoding="utf8")
+# lines = score_file.readlines()  # list 형태로 저장
+# for line in lines:
+#     print(line, end="")
+# score_file.close()
+
+### pickle
+# pickle은 항상 binary타입, encoding은 따로 정의하지 않아도 된다
+# import pickle
+
+# pickle 파일에 저장
+# profile_file = open("profile.pickle", "wb")
+# profile = {"이름":"박명수","나이":30, "취미":["축구","골프","코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) #profile에 있는 정보를 file에 저장
+# profile_file.close()
+
+# pickle 파일 불러오기
+# profile_file = open("profile.pickle", "rb")
+# profile = pickle.load(profile_file) # file에 있는 정보를 profile에 불러오기
+# print(profile)
+# profile_file.close()
+
+
+### with
+# with를 쓰면 쉽게 파일을 다룰 수 있고 매번 close를 해줄 필요도 없다
+
+# import pickle
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
+
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있어요")
+
+# with open("study.txt", "r", encoding="utf8") as study_file:
+#     print(study_file.read())
+
+"""
+
+# Quiz) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있습니다
+# 보고서는 항상 아래와 같은 형태로 출력
+
+# - X 주차 주간 보고 -
+# 부서 :
+# 이름 :
+# 업무 요약 :
+
+# 1주차부터 50주차까지의 보고서 파일을 만드는 프로그램을 작성하시오
+# 조건 : 파일명은 '1주차.txt', '2주차.txt', ... 와 같이 만듭니다.
+
+# import pickle
+# for i in range(1,5): # 50은 많아서 5까지만
+#     # with(open(str(i) + "주차.txt", "w", ... 도 가능))
+#     with open("{0}주차.txt".format(i),"w",encoding="utf8") as weekly_report:
+#         weekly_report.write("- {0} 주차 주간보고 -".format(i))
+#         weekly_report.write("\n부서 :")
+#         weekly_report.write("\n이름 :")
+#         weekly_report.write("\n업무 요약 :")
+
+
+#################### 클래스
+"""
+### 클래스
+# # 마린 : 공격 유닛, 군인, 총을 쏠 수 있음
+# name = "마린"   # 유닛의 이름
+# hp = 40         # 유닛의 체력
+# damage = 5      # 유닛의 공격력
+
+# print("{}유닛이 생성되었습니다" .format(name))
+# print("체력 {0}, 공격력 {1}\n" .format(hp, damage))
+
+# # 탱크 : 공격 유닛, 탱크, 포를 쏠 수 있는데, 일반 모드 / 시즈 모드
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
+# print("{}유닛이 생성되었습니다" .format(tank_name))
+# print("체력 {0}, 공격력 {1}\n" .format(tank_hp, tank_damage))
+
+# def attack(name, location, damage):
+#     print("{0}: {1} 방향으로 적군을 공격 합니다. [공격력 {2}]"\
+#         .format(name, location, damage))
+
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+
+"""
+# 일반 유닛
+class Unit:
+    # def __init__(self, name, hp, damage):
+    # def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
+        self.name = name
+        self.hp = hp
+        self.speed = speed
+
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
+            .format(self.name, location, self.speed))
+        # self.damage = damage
+        # print("{0} 유닛이 생성되었습니다." .format(self.name))
+        # print("체력 {0}, 공격력 {1}" .format(self.hp, self.damage))
+
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
+
+'''
+### __init__
+# 파이썬에서 쓰이는 생성자
+# marine3 = Unit("마린") # self 뒤의 파라미터를 모두 보내주어야 생성 가능
+
+### 멤버변수
+# # .name, .hp, .damage
+
+# # 레이스 : 공중 유닛, 비행기, 클로킹(상대방에게 보이지 않음)
+# wraith1 = Unit("레이스", 80, 5)
+# # 멤버변수를 외부에서 쓸 수가 있음
+# print("유닛 이름 : {0}, 공격력 : {1}" .format(wraith1.name, wraith1.damage))
+
+# # 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+# wraith2 = Unit("레이스", 80, 5)
+# # 멤버변수 확장 가능
+# # wraith1에는 추가 되지 않음
+# wraith2.clocking = True
+
+# if wraith2.clocking == True:
+# if wraith1.clocking == True:
+#     print("{0}는 현재 클로킹 상태입니다." .format(wraith2.name))
+'''
+
+### 메소드
+# 공격 유닛
+# class AttackUnit:
+#     def __init__(self, name, hp, damage):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+class AttackUnit(Unit):
+    # def __init__(self, name, hp, damage):
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name, hp, speed)
+        self.damage = damage
+
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]" \
+            .format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다." .format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1}입니다." .format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다." .format(self.name))
+
+# # 메딕 : 의무병
+
+# # 파이어뱃 : 공격 유닛, 화염방사기.
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
+
+# # 공격 2번 받는다고 가정
+# firebat1.damaged(25)
+# firebat1.damaged(25)
+
+### 상속
+
+### 다중 상속
+# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
+
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+            .format(name, location, self.flying_speed))
+
+# 공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 speed 0
+        Flyable.__init__(self, flying_speed)
+
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
+# # 발키리 : 공중 공격 유닛, 한 번에 14발 미사일 발사.
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시")
+
+### 메소드 오버라이딩
+
+# # 벌쳐 : 지상 유닛, 기동성이 좋음
+# vulture = AttackUnit("벌쳐", 80, 10, 20)
+
+# # 배틀크루저 : 공중 유닛, 체력 좋음, 공격력 좋음
+# battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+# # vulture는 move 함수인데 battlecruiser는 fly? 번거롭다
+# # 똑같이 move만 쓰면 작동하도록 '메소드 오버라이딩' 정의 가능
+# vulture.move("11시")
+# # battlecruiser.fly(battlecruiser.name, "9시")
+# battlecruiser.move("9시")
+
+
+### pass
+# pass는 일단 넘어간다
+
+# 건물
+# class BuildingUnit(Unit):
+#     def __init__(self, name, hp, location):
+#         pass
+
+# # 서플라이 디팟 : 건물, 1개 건물 = 8 유닛
+# supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+def game_over():
+    pass
+
+game_start()
+game_over()
+
+### super
+# super는 self 없이 사용
+# 단 다중상속 시 문제 발생
+class BuildingUnit(Unit):
+    def __init__(self, name, hp, location):
+        # Unit.__init__(self, name, hp, 0)
+        super().__init__(name, hp, 0) 
+        self.location = location
