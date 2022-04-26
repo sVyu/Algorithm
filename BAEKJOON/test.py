76f00952f5c6308c7498f97c18f805975ffaa3a2
vyu <vyuvyuvyu@naver.com>
2022-04-26 18:42:58 +0900

    BAEKJOON 22/04/26

--- BAEKJOON/test.py
+++ BAEKJOON/test.py
@@ -218,24 +218,318 @@ print(a*b)
 
 # 2675 문자열 반복
 
+# # a = [input().split()]
+# # print(a)
 
-# a = [input().split()]
-# print(a)
+# line_num = int(input())
 
-line_num = int(input())
+# s_list = [input().split() for _ in range(line_num)]
+# # print(s_list)
 
-s_list = [input().split() for _ in range(line_num)]
-# print(s_list)
+# for num in range(len(s_list)):
+#     out_array = list(s_list[num][1])
+#     # print(out_array)
+#     # print("haha")
 
-for num in range(len(s_list)):
-    out_array = list(s_list[num][1])
-    # print(out_array)
-    # print("haha")
+#     for num2 in range(len(out_array)):
+#         # for k in s_list[num][0]:
+#         for k in range(int(s_list[num][0])):
+#             # print("{0}, {1}" .format(num2, k))
+#             print(out_array[num2], end='')
 
-    for num2 in range(len(out_array)):
-        # for k in s_list[num][0]:
-        for k in range(int(s_list[num][0])):
-            # print("{0}, {1}" .format(num2, k))
-            print(out_array[num2], end='')
+#     print()
 
-    print()
+############################# 2022/04/26
+# 2935 소음
+# list_a = [input() for _ in range(3)]
+# # print(list_a) # 이 부분 주석처리 하는 것 까먹음
+
+# if list_a[1] == '+':
+#     print(int(list_a[0])+int(list_a[2]))
+# elif list_a[1] == '*':
+#     print(int(list_a[0])*int(list_a[2]))
+# else:
+#     print("not supported operator")
+
+# 9498 시험 성적
+# test_score = int(input())
+
+# if test_score >= 90:
+#     print("A")
+# elif test_score >= 80:
+#     print("B")
+# elif test_score >= 70:
+#     print("C")
+# elif test_score >= 60:
+#     print("D")
+# else:
+#     print("F")
+
+# 10817 세 수
+# num_list = list(map(int, input().split()))
+# # print(num_list)
+# num_list.sort()
+# # print(num_list)
+# print(num_list[-2])
+
+# 11653 소인수분해
+# num = int(input())
+
+# if num == 1:
+#     exit()
+
+# div = 2 # 나누는 수, divisor
+# num_list = []
+
+# while num > 1:
+#     if num % div == 0:
+#         num_list.append(div)
+#         num /= div
+#         continue
+
+#     elif num < div:
+#         break
+
+#     else:
+#         div += 1
+
+# # print([num_list[i] for i in range(len(num_list))])
+# for i in range(len(num_list)):
+#     print(num_list[i])
+
+# 1789 수들의 합
+# num = int(input())
+
+# sum = 0
+# ans = 0
+
+# while num >= sum:   # 등호가 있어야 함
+#     ans += 1
+#     sum += ans
+
+# ans -= 1
+# print(ans)
+
+# 2753 윤년
+# year = int(input())
+# # if year % 4 == 0 & (((year % 100) != 0) | ((year % 400) == 0)):
+# if (year % 4 == 0) & ((year % 100 != 0) | ((year % 400) == 0)):
+#     print(1)
+
+# else: print(0)
+
+# 10039 평균 점수
+# num_list = [int(input()) for _ in range(5)]
+# for x in range(5):
+#     if(num_list[x] < 40):
+#         num_list[x] = 40
+
+# print(int(sum(num_list)/5))
+
+
+# 1934 최소공배수
+# num = int(input())
+# num_list = [input().split() for _ in range(num)]
+
+# # print(num_list)
+# # 유클리드 호제법
+# def gcd (a, b):
+#     while b > 0:
+#         a, b = b, a%b
+#     return a
+
+# def lcm (a,b):
+#     return int(a * b / gcd(a,b))
+
+# for x in range(num):
+#     print(lcm(int(num_list[x][0]), int(num_list[x][1])))
+
+
+# 2480 주사위 세 개
+
+# num_list = list(map(int, input().split()))
+# num_list.sort()
+
+# if (num_list[0] == num_list[1]) & (num_list[1] == num_list[2]):
+#     print(10000 + num_list[0]*1000)
+# elif (num_list[0] == num_list[1]) | (num_list[1] == num_list[2]):
+#     print(1000 + num_list[1]*100)
+# else:
+#     print(num_list[2]*100)
+
+
+# 4101 크냐?
+# num_list = []
+# count = 0
+
+# while True:
+#     num_list.append(list(map(int, input().split())))
+#     if (num_list[count][0] == 0) & (num_list[count][1] == 0):
+#         del num_list[count]
+#         break
+#     count += 1
+
+# # print(num_list)
+# for i in range(len(num_list)):
+#     if(num_list[i][0] > num_list[i][1]):
+#         print("Yes")
+#     else:
+#         print("No")
+
+
+# 10156 과자
+# num_list = list(map(int, input().split()))
+# needed_money = num_list[0] * num_list[1] - num_list[2]
+
+# if(needed_money < 0):
+#     print(0)
+# else:
+#     print(needed_money)
+
+
+# 3009 네 번째 점
+# num_list = [list(map(int, input().split())) for _ in range(3)]
+# # print(num_list)
+# ans_list =[]
+
+# for i in range(2):
+#     if num_list[0][i] == num_list[1][i]:
+#         ans_list.append(num_list[2][i])
+
+#     elif num_list[0][i] == num_list[2][i]:
+#         ans_list.append(num_list[1][i])
+    
+#     else:
+#         ans_list.append(num_list[0][i])
+
+# print(ans_list[0], ans_list[1])
+
+
+# 2476 주사위 게임
+# num = int(input())
+# num_list = [list(map(int, input().split())) for _ in range(num)]
+# # print(num_list)
+# reward_list = []
+
+# for i in range(num):
+#     num_list[i].sort()
+#     if (num_list[i][0] == num_list[i][1]) & (num_list[i][1] == num_list[i][2]):
+#         reward_list.append(10000 + num_list[i][0] * 1000)
+#     elif (num_list[i][0] == num_list[i][1]) | (num_list[i][1] == num_list[i][2]):
+#         reward_list.append(1000 + num_list[i][1] * 100)
+#     else:
+#         reward_list.append(num_list[i][2]*100)
+
+# # print(reward_list)
+# reward_list.sort()
+# print(reward_list[-1])
+
+
+# 2754 학점계산
+# grade_dict = {  "A+":"4.3", "A0": "4.0", "A-": "3.7",\
+#                 "B+":"3.3", "B0": "3.0", "B-": "2.7",\
+#                 "C+":"2.3", "C0": "2.0", "C-": "1.7",\
+#                 "D+":"1.3", "D0": "1.0", "D-": "0.7",\
+#                 "F" :"0.0"}
+
+# # print(type(grade_dict))
+# # print(grade_dict)
+
+# grade = input()
+# print(grade_dict[grade])
+
+
+# 2884 알람 시계
+# h, m = map(int, input().split())
+# # print(h, m)
+
+# if m >= 45:
+#     m -= 45
+# else:
+#     if(h == 0):
+#         h = 24
+    
+#     h -= 1
+#     m += 15
+
+# print(h, m)
+
+
+# 7567 그릇
+# # dishes = [input()]
+# dishes = input()
+# dishes_list = []
+# # 그릇을 하나씩 분리해서 list로
+# for i in range(len(dishes)):
+#     dishes_list.append(dishes[i])
+# # print(dishes_list)
+
+# height = 10
+
+# for i in range(len(dishes_list)-1):
+#     print(i)
+#     if(dishes_list[i+1] == dishes_list[i]):
+#         height += 5
+#     else:
+#         height += 10
+
+# print(height)
+
+
+# 5063 TGN
+# num = int(input())
+# num_list = [list(map(int, input().split())) for _ in range(num)]
+# # print(num_list)
+# ad_str_list = []
+
+# for i in range(len(num_list)):
+#     benefit = (num_list[i][1] - num_list[i][0] - num_list[i][2])
+#     if benefit > 0:
+#         print("advertise")
+#     elif benefit == 0:
+#         print("does not matter")
+#     else:
+#         print("do not advertise")
+
+# 10102 개표
+# num = int(input())
+# vote = list(input())
+
+# # print(vote)
+# if vote.count('A') > (num/2):
+#     print("A")
+# elif vote.count('B') > (num/2):
+#     print("B")
+# else:
+#     print("Tie")
+
+
+# 10886     0 = not cute/ 1 = cute
+# num = int(input())
+# num_list = [int(input()) for _ in range(num)]
+
+# check = 0
+# for i in range(len(num_list)):
+#     if(num_list[i] == 1):
+#         check += 1
+
+# # 조건) num 은 홀수
+# if check > (num/2):
+#     print("Junhee is cute!")
+# else:
+#     print("Junhee is not cute!")
+
+
+# 10988 팰린드롬인지 확인하기
+word_list = list(input())
+# print(word_list)
+
+check = 1
+for i in range(int(len(word_list)/2)):
+    if(word_list[i] != word_list[-(i+1)]):
+        check = 0
+        break
+    else:
+        continue
+
+print(check)
