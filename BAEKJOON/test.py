301b6d7431a8c9ecacb3b8d94b2b0ea75e6c1495
vyu <vyuvyuvyu@naver.com>
2022-07-29 23:02:34 +0900

    daily baekjoon 2022/07/29

--- BAEKJOON/test.py
+++ BAEKJOON/test.py
@@ -7423,3 +7423,6 @@ dfs/bfs
 # n, m = map(int, input().split())
 # print(n//m, n%m, sep='\n')
 
+# 3003 킹, 퀸, 룩, 비숍, 나이트, 폰
+# king, queen, rook, bishop, knight, pawn = map(int, input().split())
+# print(1-king, 1-queen, 2-rook, 2-bishop, 2-knight, 8-pawn)
