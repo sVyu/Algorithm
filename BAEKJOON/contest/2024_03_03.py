60574b7d82686a19d9608ab3d14305f1ce1dc152
vyu <vyuvyuvyu@naver.com>
2024-03-03 23:23:30 +0900

    feat: solve BOJ 제 1회 양갈래컵 (C번까지)

---
+++ BAEKJOON/contest/2024_03_03.py
@@ -0,0 +1,22 @@
+# A번 - 갈래의 색종이 자르기
+# import math
+
+# W = int(input())
+# print(math.isqrt(W*2)*4)
+
+
+# B번 - 하늘과 핑크
+# N = int(input())
+
+# aa = sum(map(int, input().split()))
+# bb = sum(map(int, input().split()))
+# print(bb, aa)
+
+
+# C번 - 양갈래 짝 맞추기
+# N = int(input())
+
+# ans = 1
+# for n in range(N-1, 1, -2):
+#     ans *= n
+# print(ans)
