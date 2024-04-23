01945831b5e64f9f6eb511254144c1bd28576f1c
vyu <vyuvyuvyu@naver.com>
2024-04-24 01:54:42 +0900

    fix: c++ korean in/out (in vscode env)

---
+++ c_c++/korean_io_test/korean_io_test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cout << "test !!" << '\n';
+    cout << "한글 되나" << '\n';
+    cin >> s;
+    cout << s << '\n';
+    return 0;
+}
