56039801175496392b1629d3a09b79b710677b6e
vyu <vyuvyuvyu@naver.com>
2024-05-02 00:55:20 +0900

    docs: 3015 오아시스 재결합(P5) 주석 추가

--- BAEKJOON/all_problems/3K/3015.py
+++ BAEKJOON/all_problems/3K/3015.py
@@ -14,14 +14,18 @@ def solve():
     for _ in range(n):
         h = int(input())
         if(s):
+            # 오큰수와 흡사한 코드, 현재의 h보다 작은 경우 바로 ans 1 증가 등의 처리
             if(s[-1]<h):
                 while(s and s[-1]<h):
                     ans += 1
                     d[s.pop()] -= 1
                     len_s -= 1
 
+        # 4 2 2 1 2 와 같은 case가 있으면, 2 2 -- 2 에 대해서 2개만큼 더해주기 위한 코드
         if(s and s[-1]==h):
             ans += d[h]
+
+        # 2 2 2 -- 2 (더 높은 곳이 없음) 혹은 3 (단독)처럼 더할 필요 없는 경우 제외
         if(len_s > d[h]):
             ans += 1
 
