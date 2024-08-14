bdb4d63264dcf81f448d809d7864bb3610fa17a2
vyu <vyuvyuvyu@naver.com>
2024-08-15 02:49:27 +0900

    feat: solve BOJ 20352 Circus (B4)(cpp)

---
+++ BAEKJOON/all_problems/20K/20352.cpp
@@ -0,0 +1,13 @@
+# include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+using namespace std;
+
+int main(void){
+    // double a;
+    // int a;
+    long long a;
+    cin >> a;
+    cout.precision(10);
+
+    cout << 2*M_PI*sqrt(a/M_PI);
+}
