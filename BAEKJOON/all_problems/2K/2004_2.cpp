91d32edb964d0ee0aab4c477daa21493fc534590
vyu <vyuvyuvyu@naver.com>
2024-09-11 01:51:25 +0900

    feat: solve BOJ 2004 조합 0의 개수 (S2)(cpp)

---
+++ BAEKJOON/all_problems/2K/2004_2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+    ll n, m; cin >> n >> m;
+    vector<ll> bases={2, 5};
+
+    
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}
