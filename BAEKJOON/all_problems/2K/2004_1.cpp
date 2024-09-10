a3d17971ea8c3aec99e9dd813f26826fc0b0c741
vyu <vyuvyuvyu@naver.com>
2024-09-11 01:52:30 +0900

    update: BOJ 2004 조합 0의 개수 codes (S2)(cpp)

--- BAEKJOON/all_problems/2K/2004_1.cpp
+++ BAEKJOON/all_problems/2K/2004_1.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 typedef long long ll;
 
-ll cal_cnt(ll k, ll base){
-    ll cnt = 0;
-    while(k /= base) cnt += k;
-    return cnt;
+ll cal_cnt(ll &k, ll base){
+    ll val=1, cnt=0;
+    while(k >= val){
+        val *= base;
+        cnt = cnt*base+1;
+    }
+    k -= val/base;
+    return cnt/base;
 }
 
 void solve(){
@@ -16,10 +20,11 @@ void solve(){
     vector<ll> ocnts={0, 0};
 
     for(int i=0; i<bases.size(); i++){
+        ll tmpn = n, tmpm = m, tmpo = n-m;
         ll base = bases[i];
-        ncnts[i] += cal_cnt(n, base);
-        mcnts[i] += cal_cnt(m, base);
-        ocnts[i] += cal_cnt(n-m, base);
+        while(tmpn >= base) ncnts[i] += cal_cnt(tmpn, base);
+        while(tmpm >= base) mcnts[i] += cal_cnt(tmpm, base);
+        while(tmpo >= base) ocnts[i] += cal_cnt(tmpo, base);
     }
 
     cout << min(ncnts[0]-mcnts[0]-ocnts[0], ncnts[1]-mcnts[1]-ocnts[1]);
--- BAEKJOON/all_problems/2K/2004_2.cpp
+++ BAEKJOON/all_problems/2K/2004_2.cpp
@@ -2,11 +2,27 @@
 using namespace std;
 typedef long long ll;
 
+ll cal_cnt(ll k, ll base){
+    ll cnt = 0;
+    while(k /= base) cnt += k;
+    return cnt;
+}
+
 void solve(){
     ll n, m; cin >> n >> m;
     vector<ll> bases={2, 5};
+    vector<ll> ncnts={0, 0};
+    vector<ll> mcnts={0, 0};
+    vector<ll> ocnts={0, 0};
 
-    
+    for(int i=0; i<bases.size(); i++){
+        ll base = bases[i];
+        ncnts[i] += cal_cnt(n, base);
+        mcnts[i] += cal_cnt(m, base);
+        ocnts[i] += cal_cnt(n-m, base);
+    }
+
+    cout << min(ncnts[0]-mcnts[0]-ocnts[0], ncnts[1]-mcnts[1]-ocnts[1]);
 }
 
 int main(void){
