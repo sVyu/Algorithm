09bc0d1c344ea3310a133fd82c27fdb9345889be
vyu <vyuvyuvyu@naver.com>
2024-10-02 15:59:13 +0900

    feat: solve BOJ 32206 아보와 킨텍스 (S1)(cpp)

---
+++ BAEKJOON/all_problems/32K/32206.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    cout << (n+1)*26-n;
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}
