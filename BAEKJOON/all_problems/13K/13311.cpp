a170e558dd52d0869502119d61511583e46c35f0
vyu <vyuvyuvyu@naver.com>
2024-10-09 16:57:11 +0900

    feat: solve BOJ 13311 행운의 편지 (B3)(cpp & py)

---
+++ BAEKJOON/all_problems/13K/13311.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    for(int i=2; i<=100; i++){
+        cout << (-1)%i << '\n';
+    }
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}
---
+++ BAEKJOON/all_problems/13K/13311.py
@@ -0,0 +1,16 @@
+# def gcd(a, b):
+#     while b>0:
+#         a, b = b, a%b
+#     return a
+
+# def lcm(a, b):
+#     return a*b//gcd(a,b)
+
+# val = 1
+# for k in range(2, 1001):
+#     val = lcm(val, k)
+
+# print(val)
+# print(len(str(val)))
+
+print(-1)
