8b3c1316773dff84490efb72a36a7bd77edcc2a9
vyu <vyuvyuvyu@naver.com>
2024-12-29 22:01:46 +0900

    feat: solve BOJ 7891 Can you add this? (B5)(cpp)

---
+++ BAEKJOON/all_problems/7K/7891.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    while(n--){
+        int a, b; cin >> a >> b;
+        cout << a+b << '\n';
+    }
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}
