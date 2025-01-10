c8754b233e13e36b7df8e1cfd7ecfd06deafb0f6
vyu <vyuvyuvyu@naver.com>
2025-01-11 02:28:05 +0900

    feat: solve BOJ 10824 네 수 (B3)(cpp)

---
+++ BAEKJOON/all_problems/10K/10824.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(){
+    string a, b, c, d; cin >> a >> b >> c >> d;
+    cout << stoll(a+b)+stoll(c+d);
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}
