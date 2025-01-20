123510c19598ef4e4be3f7e74713ce6de9e4075e
vyu <vyuvyuvyu@naver.com>
2025-01-20 12:32:02 +0900

    feat: solve BOJ 28237 (B1)(cpp)

---
+++ BAEKJOON/all_problems/28K/28237.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+    ll n; cin >> n;
+
+    ll sqrt_n = (ll)sqrt(n);
+    ll sqrt_n_plus_2 = (ll)sqrt(n+2);
+
+    for(ll a=1; a<=sqrt_n; a++){
+        ll c=n/a;
+        if(a*c == n){
+            for(ll b=1; b<=sqrt_n_plus_2; b++){
+                if((n+2)%b == 0){
+                    ll d = ((n+2)/b);
+                    if((-b)*c+a*d == (n+1)){
+                        cout << a << ' ' << -b << ' ' << c << ' ' << d;
+                        return;
+                    }
+                    if(b*c+a*(-d) == (n+1)){
+                        cout << a << ' ' << b << ' ' << c << ' ' << -d;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    cout << -1;
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}
