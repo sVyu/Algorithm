f4d7b1eac782118c110d3782a78c3a0463609291
vyu <vyuvyuvyu@naver.com>
2025-03-31 01:05:32 +0900

    feat: solve BOJ 32982 약물 복용 (S2)(js)

---
+++ BAEKJOON/all_problems/32K/32982.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const inputData = fs.readFileSync(0, 'utf-8').trim().split('\n');
+
+const [n, k] = inputData[0].split(' ').map(Number);
+const zs = inputData[1].split(' ').map(Number);
+
+const se_times = [];
+for (let i = 0; i < 3; ++i) {
+  se_times.push([zs[2 * i], zs[2 * i + 1]]);
+}
+// console.log(se_times);
+
+// mct : mwt
+let mwt = se_times[0][1];
+for (let day = 1; day <= n; ++day) {
+  for (let i = 0; i < 3; ++i) {
+    if (day == 1 && i == 0) continue;
+
+    mwt += k;
+    if (mwt < se_times[i][0] + (day - 1) * 1440) {
+      console.log('NO');
+      return;
+    }
+    mwt = Math.min(mwt, se_times[i][1] + (day - 1) * 1440);
+    // console.log(base);
+  }
+}
+console.log('YES');
